72668e8b877df73b74b3f9b27e35f342145c41f6
Carsten Pettersson <carsten@petterssons.dk>
2018-05-16 15:53:57 +0200

    Update common-system-settings.sh

--- bin/common-system-settings.sh
+++ bin/common-system-settings.sh
@@ -3,14 +3,14 @@
 ## macOS Preferences
 
 # Set hostname
-scutil --set HostName "MacMini"
+scutil --set HostName "BigMac"
 
 # Set a blazingly fast keyboard repeat rate
 
 defaults write NSGlobalDomain KeyRepeat -int 4
 
 # Set a shorter Delay until key repeat
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Add a context menu item for showing the Web Inspector in web views
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
